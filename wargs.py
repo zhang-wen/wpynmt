271aed403d16782cd45620e0215a6dba3539b3ef
zhang-wen <wzhang.xt@gmail.com>
2018-02-06 21:18:12 +0800

    small fix

--- wargs.py
+++ wargs.py
@@ -1,4 +1,4 @@
-dataset = 'L' # S for 40k, M for 1.2M, L for wmt en-de
+dataset = 'M' # S for 40k, M for 1.2M, L for wmt en-de
 
 # Maximal sequence length in training data
 #max_seq_len = 10000000
@@ -69,6 +69,7 @@ epoch_shuffle = False
 epoch_shuffle_minibatch = 1
 
 small = False
+eval_small = False
 epoch_eval = False
 final_test = False
 
@@ -90,7 +91,7 @@ if dataset == 'S':
     max_epochs = 60
     epoch_eval = True
     small = True
-    use_multi_bleu = True
+    use_multi_bleu = False
     cased = False
 elif dataset == 'M':
     src_wemb_size = 512
@@ -99,16 +100,16 @@ elif dataset == 'M':
     align_size = 512
     dec_hid_size = 512
     out_size = 512
-    #val_tst_dir = '/home5/wen/2.data/mt/nist_data_stanseg/'
-    val_tst_dir = '/home/wen/3.corpus/mt/nist_data_stanseg/'
+    val_tst_dir = '/home5/wen/2.data/mt/nist_data_stanseg/'
+    #val_tst_dir = '/home/wen/3.corpus/mt/nist_data_stanseg/'
     val_prefix = 'nist02'
     dev_prefix = 'nist02'
-    #@val_src_suffix = '8kbpe.src'
-    val_src_suffix = 'src'
+    val_src_suffix = '8kbpe.src'
+    #val_src_suffix = 'src'
     val_ref_suffix = 'ref.plain_'
     ref_cnt = 4
     tests_prefix = ['nist03', 'nist04', 'nist05', 'nist06', 'nist08', '900']
-    with_bpe = False
+    with_bpe = True
     with_postproc = True
     use_multi_bleu = False
     cased = False
@@ -121,23 +122,18 @@ elif dataset == 'L':
     #out_size = 512
     #val_tst_dir = '/home/wen/3.corpus/wmt16/rsennrich/devtst/'
     #val_tst_dir = '/home/wen/3.corpus/wmt14/en-de-Luong/'
-    #val_tst_dir = '/home/wen/3.corpus/wmt2017/de-en/'
-    #val_prefix = 'newstest2014'
+    val_tst_dir = '/home/wen/3.corpus/wmt2017/de-en/'
+    val_prefix = 'newstest2014'
     #val_prefix = 'newstest2014.tc'
-    #val_src_suffix = 'en.16kbpe'
-    #val_ref_suffix = 'ori.de'
-    val_tst_dir = '/home/wen/3.corpus/wmt14/en-fr/jean/dev/'
-    val_prefix = 'ntst1213'
-    val_src_suffix = 'en'
-    val_ref_suffix = 'fr'
+    val_src_suffix = 'en.16kbpe'
+    val_ref_suffix = 'ori.de'
     ref_cnt = 1
-    tests_prefix = ['ntst14']
-    #tests_prefix = ['newstest2014.2737', 'newstest2015', 'newstest2016', 'newstest2017']
+    tests_prefix = ['newstest2014.2737', 'newstest2015', 'newstest2016', 'newstest2017']
     #tests_prefix = ['newstest2009', 'newstest2010', 'newstest2011', 'newstest2012', 'newstest2014', 'newstest2015', 'newstest2016', 'newstest2017']
     #drop_rate = 0.2
-    src_dict_size = 30000
-    trg_dict_size = 30000
-    with_bpe = False
+    src_dict_size = 50000
+    trg_dict_size = 50000
+    with_bpe = True
     use_multi_bleu = True
     cased = True    # False: Case-insensitive BLEU  True: Case-sensitive BLEU
 
@@ -145,9 +141,8 @@ display_freq = 10 if small else 1000
 sampling_freq = 100 if small else 5000
 sample_size = 5
 if_fixed_sampling = False
-eval_small = False
-eval_valid_from = 50 if eval_small else 50000
-eval_valid_freq = 10 if eval_small else 20000
+eval_valid_from = 50000 if eval_small else 100000
+eval_valid_freq = 10000 if eval_small else 20000
 
 save_one_model = True
 start_epoch = 1
@@ -165,7 +160,7 @@ with_batch = 1
 ori_search = 0
 beam_size = 10
 vocab_norm = 1  # softmax
-len_norm = 1    # 0: no noraml, 1: length normal, 2: alpha-beta
+len_norm = 2    # 0: no noraml, 1: length normal, 2: alpha-beta
 with_mv = 0
 merge_way = 'Y'
 avg_att = 0
@@ -194,7 +189,7 @@ start_decay_from = None
 learning_rate_decay = 0.5
 last_valid_bleu = 0.
 
-snip_size = 10
+snip_size = 1
 file_tran_dir = 'wexp-gpu-nist03'
 laynorm = False
 segments = False
@@ -222,9 +217,9 @@ print_att = True
 
 # Scheduled Sampling of Samy bengio's paper
 bleu_sampling = False
-ss_type = None     # 1: linear decay, 2: exponential decay, 3: inverse sigmoid decay
-ss_eps_begin = 1   # set None for no scheduled sampling
-ss_eps_end = 1
+ss_type = 3     # 1: linear decay, 2: exponential decay, 3: inverse sigmoid decay
+ss_eps_begin = 1.   # set None for no scheduled sampling
+ss_eps_end = 1.
 #ss_decay_rate = 0.005
 ss_decay_rate = (ss_eps_begin - ss_eps_end) / 10.
 ss_k = 12.     # k < 1 for exponential decay, k >= 1 for inverse sigmoid decay
@@ -239,6 +234,6 @@ sampling = 'length_limit'     # truncation, length_limit, gumbeling
 #tests_prefix = None
 #dec_gpu_id = [1]
 #dec_gpu_id = None
-gpu_id = [1]
+gpu_id = [2]
 #gpu_id = None
 
